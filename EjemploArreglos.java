874ca587dd1043c7dbcc8fed59ac913f9fc83db1
Sebastian Alejos <bastialejos.1997@gmail.com>
2019-08-30 12:59:14 -0500

    Metodo suma maximo e imprime

--- EjemploArreglos.java
+++ EjemploArreglos.java
@@ -1,5 +1,40 @@
 public class EjemploArreglos{
 	public int suma(int[] arreglo){
-		for (int i=0;)
+		int res=0;
+		for (int i=0;i<arreglo.length;i++){
+			res+=arreglo[i];
+		}
+		return res;
+	}
+
+	public int maximo(int[] arreglo){
+		int max=arreglo[0];
+		for (int i=0;i<arreglo.length;i++){
+			if(arreglo[i]>max){
+				max=arreglo[i];
+			}
+		}
+		return max;
+	}
+
+	public void imprime(String[][] palabras){
+		for (int i=0;i<palabras.length;i++){
+			for(int j=0;j<palabras.length;j++){
+				System.out.println(palabras[i][j]);
+			}
+		}
+	}
+
+	public static void main(String[] args){
+		EjemploArreglos ea=new EjemploArreglos();
+		int [] valores={-4,-16,-8,-144,-2};
+		int valor= ea.suma(valores);
+		System.out.println(valor);
+		int maximo= ea.maximo(valores);
+		System.out.println(maximo);
+		for(int i=0;i<args.length;i++){
+			System.out.println("Args posición "+ i + ": "+ args[i]);
+		}
+		String[][] nombres={{}{}}
 	}
 }
