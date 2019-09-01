dd434be38116dfd5d6887ad9ff874314fabdb6d0
Sebastian Alejos <bastialejos.1997@gmail.com>
2019-09-01 17:07:12 -0500

    Se añaden métodos de la clase

---
+++ Vector.java
@@ -0,0 +1,66 @@
+public class Vector{
+    private double i,
+                   j, 
+                   k;
+    
+    public Vector(){
+        this.i=0;
+        this.j=0;
+        this.k=0;
+    }
+    
+    public Vector(double x, double y, double z){
+        this.i=x;
+        this.j=y;
+        this.k=z;
+    }
+    
+    public double magnitud(){
+        double mag=Math.sqrt((this.i*this.i)+(this.j*this.j)+(this.k*this.k));
+        return mag;
+    }
+    
+    public Vector suma(Vector v){
+        double xs=this.i+v.i;
+        double ys=this.j+v.j;
+        double zs=this.k+v.k;
+        Vector vs=new Vector(xs,ys,zs);
+        return vs;
+    }
+
+    public Vector resta(Vector v){
+        double xs=this.i-v.i;
+        double ys=this.j-v.j;
+        double zs=this.k-v.k;
+        Vector vs=new Vector(xs,ys,zs);
+        return vs;
+    }
+
+    public double productoPunto (Vector v){
+        double pp=(this.i*v.i)+(this.j*v.j)+(this.k*v.k);
+        return pp;
+    }
+
+    public Vector productoCruz (Vector v){
+        double xs=(this.j*v.k)-(this.k*v.j);
+        double ys=(this.k*v.i)-(this.i*v.k);
+        double zs=(this.i*v.j)-(this.j*v.i);
+        Vector vs=new Vector(xs,ys,zs);
+        return vs;
+    } 
+
+    public void agregar(Vector v){
+        this.i=v.i;
+        this.j=v.j;
+        this.k=v.k;
+    }
+
+    public void imprimeVector(){
+        System.out.println("("+this.i+" , "+this.j+" , "+this.k+")");
+    }
+    public static void main(String[] args) {
+        Vector a=new Vector(1,2,3);
+        Vector b=new Vector(3,4,5);
+        a.imprimeVector();
+    }  
+}
