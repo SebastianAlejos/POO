618da56268e8672589d00e8f800e3299e7d5ae87
Sebastian Alejos <bastialejos.1997@gmail.com>
2019-09-10 12:42:54 -0500

    Rectangulo

--- Figura.java
+++ Figura.java
@@ -1,6 +1,32 @@
 public class Figura{
-    
+    public double perimetro(){
+    return 0.0;
+    }
+
+    public double area(){
+        return 0.0;
+    }
+
+    public double volumen(){
+        return 0.0;
+    }
+
+    public String toString(){
+        return "Figura abstracta";
+    }
     public static void main(String[] args) {
-        
+        Figura a=new Figura();
+        System.out.println(a);
+        System.out.println("El perimetro es "+a.perimetro());
+        System.out.println("El area es "+a.area());
+        System.out.println("El volumen es "+a.volumen());
+        System.out.println();
+
+
+        Rectangulo b=new Rectangulo();
+        System.out.println(b);
+        System.out.println("El perimetro es "+b.perimetro());
+        System.out.println("El area es "+b.area());
+        System.out.println("El volumen es "+b.volumen());
     }
 }
---
+++ Rectangulo.java
@@ -0,0 +1,25 @@
+public class Rectangulo extends Figura{
+    private double largo,   
+                            ancho;
+
+    public Rectangulo(double largo, double ancho){
+        this.ancho=ancho;
+        this.largo=largo;
+    }
+
+    public Rectangulo(){
+        this(10,4);
+    }
+    
+    public double perimetro(){
+        return 2.0*(this.largo+this.ancho);
+    }
+
+    public double area(){
+        return this.largo*this.ancho;
+    }
+    public String toString(){
+        return "Rectangulo con largo "+this.largo +" y ancho "+this.ancho;
+    }
+
+}
